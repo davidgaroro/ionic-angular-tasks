2bfdf9a57295918d5e516c06cd0dcba33629c140
davidgaroro <davidgaroro@gmail.com>
2019-05-06 21:41:16 +0200

    Add get tasks from Firestore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TaskService } from '../tasks/shared/task.service';
+import { TaskId } from '../tasks/shared/task';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  tasks: Observable<TaskId[]>;
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Task, TaskId } from './task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private tasksCollection: AngularFirestoreCollection<Task>;
+
+  constructor(private readonly db: AngularFirestore) {
+    this.tasksCollection = this.db.collection<Task>('tasks');
+  }
+
+  //////// Tasks methods ////////
+
+    /** GET: tasks from Firestore */
+    getTasks(): Observable<TaskId[]> {
+      return this.db.collection<Task>('tasks', ref => 
+        ref.orderBy('modified', 'desc').limit(30)
+      )
+      .snapshotChanges().pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data() as Task;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        }))
+      );
+    }
 }
