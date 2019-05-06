5b904b3d98a8ff0125a73fe80bed05bd475e6899
davidgaroro <davidgaroro@gmail.com>
2019-05-06 21:12:29 +0200

    Install and setup AngularFire and Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebaseConfig } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // imports firebase/app needed for everything
+    AngularFirestoreModule // imports firebase/firestore, only needed for database features
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAvGbFbkPCI3gGMS8w0n6ouqHzN_h64Oxg',
+  authDomain: 'tasks-backend-7.firebaseapp.com',
+  databaseURL: 'https://tasks-backend-7.firebaseio.com',
+  projectId: 'tasks-backend-7',
+  storageBucket: 'tasks-backend-7.appspot.com',
+  messagingSenderId: '803834969846',
+  appId: '1:803834969846:web:7398cebfcd0b9799'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
