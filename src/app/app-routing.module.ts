62ed9fc6f0330dc39120ba978e781f1639aba628
davidgaroro <davidgaroro@gmail.com>
2019-05-07 19:18:37 +0200

    Add redirect 404 to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'task/:id', loadChildren: './tasks/task/task.module#TaskPageModule' },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
