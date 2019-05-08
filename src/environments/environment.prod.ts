4d196e3dec5b88acb235d6a35b8ab56e3fe20dc6
davidgaroro <davidgaroro@gmail.com>
2019-05-08 02:21:47 +0200

    Add firebase config in enviroment.prod.ts to use ionic build --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAvGbFbkPCI3gGMS8w0n6ouqHzN_h64Oxg',
+  authDomain: 'tasks-backend-7.firebaseapp.com',
+  databaseURL: 'https://tasks-backend-7.firebaseio.com',
+  projectId: 'tasks-backend-7',
+  storageBucket: 'tasks-backend-7.appspot.com',
+  messagingSenderId: '803834969846',
+  appId: '1:803834969846:web:7398cebfcd0b9799'
+};
